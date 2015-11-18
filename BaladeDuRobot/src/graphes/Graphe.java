e21d70d6fee99fd434d8e7a4c81610cca7e7a611
hippunk <arthur.ramolet@hotmail.fr>
2015-11-18 16:57:10 +0100

    Ajout petit test GraphStream

--- BaladeDuRobot/src/graphes/Graphe.java
+++ BaladeDuRobot/src/graphes/Graphe.java
@@ -1,9 +1,25 @@
 package graphes;
+import metier.Entrepot;
+import metier.Instance;
+import metier.Robot;
+
 import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.SingleGraph;
 
 public class Graphe {
-
+	private Instance instance;
+	private Graph graph;
 	
+	public Graphe(Instance instance){
+		this.instance = instance;
+
+	}
+	
+	public void afficher(){
+
+
+
+	}
 	
 	
 }
